bdb5f61959e1270bb28d349a2bc06207b5d13212
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-04-12 02:52:16 +0600

    header and footer added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Shared/Header/Header';
+import Footer from './Components/Shared/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <h3>hellow World</h3>
+    <div className="App min-h-screen flex flex-col  justify-between ">
+      <Header></Header>
+      
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Components/Shared/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <header className='flex justify-between px-10 py-5 bg-blue-100'>
+
+            <h2 className='font-bold text-2xl text-slate-600'>Book Hotel Room</h2>
+            <ul className='flex gap-5'>
+                <NavLink className="font-semibold text-slate-600" style={({isActive})=>isActive?{color:'goldenrod'}:{}} to='/'>Home</NavLink>
+                <NavLink className="font-semibold text-slate-600" style={({isActive})=>isActive?{color:'goldenrod'}:{}}  to='/room'>Room</NavLink>
+                <NavLink className="font-semibold text-slate-600" style={({isActive})=>isActive?{color:'goldenrod'}:{}}  to='/about'>About</NavLink>
+                <NavLink className="font-semibold text-slate-600" style={({isActive})=>isActive?{color:'goldenrod'}:{}}  to='/login'>Login</NavLink>
+            </ul>
+        </header>
+    );
+};
+
+export default Header;
